32aef843b8fda41a3109f89561155d16f13c48b1
Gaurav Nayal <nayalg2003@gmail.com>
2026-06-24 19:24:01 +0530

    Merge-Sort

---
+++ MergeSort.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(int arr[],int start,int mid,int end){
+    vector<int>temp(end-start+1);
+    int left=start,right=mid+1,index = 0;
+    while(left<=mid && right<=end){
+        if(arr[left]<=arr[right]){
+            temp[index] = arr[left];
+            index++,left++;
+        }else{
+            temp[index] = arr[right];
+            index++,right++;
+        }
+    }
+    //If element remaining  in left array
+        while(left<=mid){
+            temp[index] = arr[left];
+            index++,left++;
+        }
+        //If element remaining in right array
+        while(right<=end){
+            temp[index] = arr[right];
+            index++,right++;
+        }
+        
+    //Put these value in array
+    index = 0;
+    while(start<=end){
+        arr[start] = temp[index];
+        start++,index++;
+    }
+}
+
+
+void mergesort(int arr[],int start,int end){
+
+    if(start==end){
+        return;
+    }
+    int mid = start+(end-start)/2;
+    //left side
+    mergesort(arr,start,mid);
+    //Right side
+    mergesort(arr,mid+1,end);
+    merge(arr,start,mid,end);
+}
+
+int main(){
+     int arr[] = {6,3,1,2,8,9,10,7,3,10};
+     mergesort(arr,0,9);
+     for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+     }
+}

0d6db65fe4e1d841f054d4a4f7d005220efe0278
Gaurav Nayal <nayalg2003@gmail.com>
2026-07-16 19:12:17 +0530

    linked list

--- Linked list/linkedlist.cpp
+++ Linked list/linkedlist.cpp
@@ -17,8 +17,37 @@ int main(){
 
     //Creates dynamically
     Node *Head;
-    Head = new Node(4);
+    // Head = new Node(4);
 
-    cout<<Head->data<<endl;
-    cout<<Head->next<<endl;
+    Head = NULL;
+
+    // cout<<Head->data<<endl;
+    // cout<<Head->next<<endl;
+
+    //Insert node at beginning
+
+    int arr[] = {2,4,6,8,10};
+    //Linked list doesnt exist
+
+    for(int i=0;i<5;i++){
+    if(Head==NULL){
+        // Head = new Node(5);
+        Head = new Node(arr[i]);
+    }
+    //Linked list exist
+    else{
+        Node *temp;
+        // temp = new Node(5);
+        temp = new Node(arr[i]);
+        temp->next = Head;
+        Head = temp;
+    }
+}
+
+//Print the value
+Node *temp = Head;
+while(temp!=NULL){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+}
 }
---
+++ Linked list/practice.cpp
@@ -0,0 +1,71 @@
+// #include<iostream>
+// using namespace std;
+
+// struct Node{
+//     int data;
+//     Node *next;
+
+//     Node(int value){
+//         data = value;
+//         next = NULL;
+//     }
+// };
+
+// int main(){
+//     Node *Head;
+//     Head = new Node(4);
+
+//     Head->next = new Node(8);
+//     Head->next->next = new Node(25);
+
+//     Node *temp = Head;
+//     while(temp!=NULL){
+//         cout<<temp->data<<" -> ";
+//         temp = temp->next;
+//     }
+//     cout<<"NULL";
+
+//     // cout<<Head->data<<" -> ";
+//     // cout<<Head->next->data<<" ->  ";
+//     // cout<<Head->next->next->data<<" -> ";
+//     // cout<<"NULL";
+// }
+
+
+//Insert at beginning
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+
+
+int main(){
+    Node *Head;
+    Head = new Node(5);
+
+    Head->next = new Node(10);
+    Head->next->next = new Node(25);
+
+    //Insert at beginning
+    Node *newnode = new Node(2);
+
+    newnode->next = Head;
+    Head = newnode;
+
+    Node *temp = Head;
+    while(temp!=NULL){
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL";
+}

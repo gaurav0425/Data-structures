57e9234bbed103e7d96b81c0bc1b9e6bc2546954
Gaurav Nayal <nayalg2003@gmail.com>
2026-07-17 18:12:45 +0530

    dsa

--- Linked list/linkedlist.cpp
+++ Linked list/linkedlist.cpp
@@ -1,53 +1,106 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node *next;
+
+//     Node(int value){
+//         data = value;
+//         next = NULL;
+//     }
+// };
+
+// int main(){
+//     // Node A1(4);
+
+//     //Creates dynamically
+//     Node *Head;
+//     // Head = new Node(4);
+
+//     Head = NULL;
+
+//     // cout<<Head->data<<endl;
+//     // cout<<Head->next<<endl;
+
+//     //Insert node at beginning
+
+//     int arr[] = {2,4,6,8,10};
+//     //Linked list doesnt exist
+
+//     for(int i=0;i<5;i++){
+//     if(Head==NULL){
+//         // Head = new Node(5);
+//         Head = new Node(arr[i]);
+//     }
+//     //Linked list exist
+//     else{
+//         Node *temp;
+//         // temp = new Node(5);
+//         temp = new Node(arr[i]);
+//         temp->next = Head;
+//         Head = temp;
+//     }
+// }
+
+// //Print the value
+// Node *temp = Head;
+// while(temp!=NULL){
+//     cout<<temp->data<<" ";
+//     temp=temp->next;
+// }
+// }
+
+// Insert node at end
+
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
 
-    Node(int value){
+    Node(int value)
+    {
         data = value;
         next = NULL;
     }
 };
 
-int main(){
-    // Node A1(4);
-
-    //Creates dynamically
-    Node *Head;
-    // Head = new Node(4);
-
+int main()
+{
+    Node *Head, *tail;
+    tail = NULL;
     Head = NULL;
 
-    // cout<<Head->data<<endl;
-    // cout<<Head->next<<endl;
+    int arr[] = {2, 4, 6, 8, 10};
 
-    //Insert node at beginning
+    for (int i = 0; i < 5; i++)
+    {
 
-    int arr[] = {2,4,6,8,10};
-    //Linked list doesnt exist
-
-    for(int i=0;i<5;i++){
-    if(Head==NULL){
-        // Head = new Node(5);
-        Head = new Node(arr[i]);
+        // linked list is empty
+        if (Head == NULL)
+        {
+            Head = new Node(arr[i]);
+            tail = Head;
+        }
+        // linked list exist
+        else
+        {
+            tail->next= new Node(arr[i]);
+            tail = tail->next;
+        }
     }
-    //Linked list exist
-    else{
-        Node *temp;
-        // temp = new Node(5);
-        temp = new Node(arr[i]);
-        temp->next = Head;
-        Head = temp;
-    }
-}
 
-//Print the value
-Node *temp = Head;
-while(temp!=NULL){
-    cout<<temp->data<<" ";
-    temp=temp->next;
-}
+    Node *temp;
+    temp = Head;
+
+    while(temp!=NULL){
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL";
 }

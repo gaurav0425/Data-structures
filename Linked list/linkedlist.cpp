b310477e103a56809317b7267f01f6077211a254
Gaurav Nayal <nayalg2003@gmail.com>
2026-07-19 18:41:44 +0530

    data structure

--- Linked list/linkedlist.cpp
+++ Linked list/linkedlist.cpp
@@ -52,55 +52,166 @@
 // }
 // }
 
+
+
+
+
 // Insert node at end
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int data;
+//     Node *next;
+
+//     Node(int value)
+//     {
+//         data = value;
+//         next = NULL;
+//     }
+// };
+
+// int main()
+// {
+//     Node *Head, *tail;
+//     tail = NULL;
+//     Head = NULL;
+
+//     int arr[] = {2, 4, 6, 8, 10};
+
+//     for (int i = 0; i < 5; i++)
+//     {
+
+//         // linked list is empty
+//         if (Head == NULL)
+//         {
+//             Head = new Node(arr[i]);
+//             tail = Head;
+//         }
+//         // linked list exist
+//         else
+//         {
+//             tail->next= new Node(arr[i]);
+//             tail = tail->next;
+//         }
+//     }
+
+//     Node *temp;
+//     temp = Head;
+
+//     while(temp!=NULL){
+//         cout<<temp->data<<" -> ";
+//         temp = temp->next;
+//     }
+//     cout<<"NULL";
+// }
+
+
+
+
+
+
+//Create Llinked list using recursion place node at end
+
+// #include<iostream>
+// using namespace std;
+
+// class Node{
+//     public: 
+//     int data;
+//     Node *next;
+
+//     Node(int value){
+//         data = value;
+//         next = NULL;
+//     }
+
+// };
+
+// Node* CreateLinkedList(int arr[],int index,int size){
+//     //Base case
+//     if(index==size){
+//         return NULL;
+//     }
+
+//     Node *temp;
+//     temp = new Node(arr[index]);
+//     temp->next = CreateLinkedList(arr,index + 1,size);
+
+//     return temp;
+// }
+
+// int main(){
+//     Node *Head;
+//     Head = NULL;
+
+//     int arr[] = {2,4,6,8,10};
+
+//    Head = CreateLinkedList(arr,0,5);
+
+//    Node *temp;
+//    temp = Head;
+
+//    while(temp !=NULL){
+//     cout<<temp->data<<" -> ";
+//     temp = temp->next;
+//    }
+//    cout<<"NULL";
+// }
+
+
+
+
+
+//Insert at beginning using recursion
+
+
+#include<iostream>
 using namespace std;
 
-class Node
-{
-public:
+class Node{
+    public:
     int data;
     Node *next;
 
-    Node(int value)
-    {
-        data = value;
+    Node(int value){
+        data= value;
         next = NULL;
     }
 };
 
-int main()
-{
-    Node *Head, *tail;
-    tail = NULL;
-    Head = NULL;
-
-    int arr[] = {2, 4, 6, 8, 10};
-
-    for (int i = 0; i < 5; i++)
-    {
-
-        // linked list is empty
-        if (Head == NULL)
-        {
-            Head = new Node(arr[i]);
-            tail = Head;
-        }
-        // linked list exist
-        else
-        {
-            tail->next= new Node(arr[i]);
-            tail = tail->next;
-        }
+Node *CreateLinkedList(int arr[],int index,int size,Node *prev){
+    //Base case
+    if(index==size){
+        return prev;
     }
 
     Node *temp;
-    temp = Head;
+    temp = new Node(arr[index]);
+    temp->next = prev;
 
+    return CreateLinkedList(arr,index+1,size,temp);
+}
+
+
+int main(){
+    Node *Head;
+    Head = NULL;
+
+    int arr[] = {2,4,6,8,10};
+
+   Head = CreateLinkedList(arr,0,5,Head);
+
+    //Print
+    Node *temp ;
+    temp = Head;
     while(temp!=NULL){
         cout<<temp->data<<" -> ";
         temp = temp->next;
     }
+
     cout<<"NULL";
 }
--- Linked list/practice.cpp
+++ Linked list/practice.cpp
@@ -34,6 +34,47 @@
 
 //Insert at beginning
 
+// #include<iostream>
+// using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node *next;
+
+//     Node(int value){
+//         data = value;
+//         next = NULL;
+//     }
+// };
+
+
+// int main(){
+//     Node *Head;
+//     Head = new Node(5);
+
+//     Head->next = new Node(10);
+//     Head->next->next = new Node(25);
+
+//     //Insert at beginning
+//     Node *newnode = new Node(2);
+
+//     newnode->next = Head;
+//     Head = newnode;
+
+//     Node *temp = Head;
+//     while(temp!=NULL){
+//         cout<<temp->data<<" -> ";
+//         temp = temp->next;
+//     }
+//     cout<<"NULL";
+// }
+
+
+
+
+//Insert at end
+
 #include<iostream>
 using namespace std;
 
@@ -42,30 +83,35 @@ class Node{
     int data;
     Node *next;
 
+
     Node(int value){
         data = value;
         next = NULL;
     }
 };
 
-
 int main(){
-    Node *Head;
-    Head = new Node(5);
+    Node *Head,*tail;
+    Head = NULL;
+    tail = NULL;
 
-    Head->next = new Node(10);
-    Head->next->next = new Node(25);
+    int arr[] = {2,4,6,8,10};
 
-    //Insert at beginning
-    Node *newnode = new Node(2);
-
-    newnode->next = Head;
-    Head = newnode;
-
-    Node *temp = Head;
-    while(temp!=NULL){
-        cout<<temp->data<<" -> ";
-        temp = temp->next;
+   for(int i=0;i<5;i++){
+    if(Head==NULL){
+        Head = new Node(arr[i]);
+        tail = Head;
+    }else{
+        tail->next = new Node(arr[i]);
+        tail = tail->next;
     }
-    cout<<"NULL";
+   }
+   Node *temp;
+   temp = Head;
+
+   while(temp!=NULL){
+    cout<<temp->data<<" -> ";
+    temp = temp->next;
+   }
+   cout<<"NULL";
 }
